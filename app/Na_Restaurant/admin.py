2faf6d9daec9124c878be0edc958b309527dd90f
yumin <elin0225@naver.com>
2020-09-08 07:57:31 +0900

    image 설정 추가

--- app/Na_Restaurant/admin.py
+++ app/Na_Restaurant/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Restaurant
 # Register your models here.
 
 class RestaurantAdmin(admin.ModelAdmin):
-    list_display =['restaurant_id', 'restaurant_name', 'latitude', 'longitude', 'restaurant_address']
+    list_display = ['restaurant_id', 'restaurant_name', 'latitude', 'longitude', 'restaurant_address', 'image_1', 'image_tag1', 'image_2', 'image_tag2']
+
+    def image_tag1(self, obj):
+        return format_html('<img src="../../../media/{}" width="50px;"/>'.format(obj.image_1))
+
+    def image_tag2(self, obj):
+        return format_html('<img src="../../../media/{}" width="50px;"/>'.format(obj.image_2))
+
 
 admin.site.register(Restaurant, RestaurantAdmin)
--- app/Na_Restaurant/urls.py
+++ app/Na_Restaurant/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import CreateRestaurantAPI, UpdateRestaurantAPI, DestroyRestaurantAPI, RetrieveRestaurantAPI
 
 urlpatterns = format_suffix_patterns([
@@ -9,4 +7,4 @@ urlpatterns = format_suffix_patterns([
     url(r'^(?P<pk>\d+)/updaterestaurant/$', UpdateRestaurantAPI.as_view()),
     url(r'^(?P<pk>\d+)/destroyrestaurant/$', DestroyRestaurantAPI.as_view()),
     url(r'^(?P<pk>\d+)/retrieverestaurant/$', RetrieveRestaurantAPI.as_view()),
-]) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+])
